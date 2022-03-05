503d5255169ad3feec2b441ed360315a247eeb1d
silentlight <526416486@qq.com>
2022-03-05 14:17:08 +0800

    使用老师提供的在线接口

--- src/network/request.js
+++ src/network/request.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export function request(config){
     const instance = axios.create({
-        // baseURL: 'http://123.207.32.32:8000',
+        baseURL: 'http://152.136.185.210:7878/api/hy66/',
         // 使用别人搭建好的本地接口
-        baseURL: 'http://localhost:3000/',
+        // baseURL: 'http://localhost:3000/',
         timeout: 5000
     })
     // /* 请求拦截器 */
